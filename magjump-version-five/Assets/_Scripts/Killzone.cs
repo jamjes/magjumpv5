bb97d117d6960940d8bc7028782b08e0e970caad
Jam <77445181+jamjes@users.noreply.github.com>
2025-01-21 19:00:21 +0000

    // Player Mechanics Polished
    1. Refactored jump mechanic
    - Player impulse is no longer tied to the platform pole. This makes the control feel more responsive. Instead of two separate keys for the same functionality, 1 key will function both ways.
    
    2. Screen Shake
    Added a screenshake to Repel, Attract and Newly Grounded. This communicates to the player the impact caused by the magnet mechanic.

--- magjump-version-five/Assets/_Scripts/Killzone.cs
+++ magjump-version-five/Assets/_Scripts/Killzone.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class Killzone : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.collider.tag == "Player") {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            StartCoroutine(DelayScene(SceneManager.GetActiveScene().buildIndex));
         }
     }
+
+    private IEnumerator DelayScene(int sceneIndex) {
+        yield return new WaitForSeconds(.3f);
+        SceneManager.LoadScene(sceneIndex);
+    }
 }
+
+
--- magjump-version-five/Assets/_Scripts/Player.cs
+++ magjump-version-five/Assets/_Scripts/Player.cs
@@ -1,62 +1,58 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private LayerMask platformLayer;
-    [SerializeField] private Pivot pivotReference;
-    [SerializeField] private float detectionRadius;
-    [SerializeField] private float power;
+    [SerializeField] private LayerMask platformLayer; //Reference to magentised platforms
+    [SerializeField] private Pivot pivotReference; //Reference to UI pointer
+    [SerializeField] private float detectionRadius; //Magnetic strength
+    [SerializeField] private float power; //Impulse force
+    [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private Shake shake;
+    private float groundTimeRef;
+    [SerializeField] private BoxCollider2D boxColl;
 
-    private Rigidbody2D rigidBody;
-
-    private void Awake() {
-        rigidBody = GetComponent<Rigidbody2D>();
+    private void Start() {
+        groundTimeRef = Time.time;
     }
 
     private void Update() {
         Vector2 direction = pivotReference.GetDirection();
-
-        if (Input.GetKeyDown(KeyCode.X)) { //Input.GetMouseButtonDown(1)
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, detectionRadius, platformLayer);
-            if (hit.collider != null) {
-                direction = CalculateDirection(pivotReference.GetAngle());
-                if (hit.collider.tag == "South") {
-                    Repel(direction * -1);
-                }
-   
-            }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, detectionRadius, platformLayer); //Player is within range to attract / repel
+        
+        if (Input.GetKeyDown(KeyCode.X) && hit.collider != null) {
+            direction = ClampDirection(pivotReference.GetAngle());
+            int impulseMode = direction.y < 0 ? -1 : 1; //If direction is down, repel, else, attract
+            Impulse(direction, impulseMode);
         }
 
-        if (Input.GetKeyDown(KeyCode.Z)) {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, detectionRadius, platformLayer);
-            if (hit.collider != null) {
-                direction = CalculateDirection(pivotReference.GetAngle());
-                if (hit.collider.tag == "North") {
-                    Attract(direction);
-                }
-
-            }
-        }
-
-        if (Input.GetKey(KeyCode.Z)) {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, .5f, platformLayer);
-            if (hit.collider != null) {
+        if (Input.GetKey(KeyCode.X)) {
+            RaycastHit2D topHit = Physics2D.Raycast(transform.position, Vector2.up, .5f, platformLayer); //If player is holding magnetic key and platform is above player
+            if (topHit.collider != null) {
                 rigidBody.gravityScale = 0;
                 rigidBody.linearVelocity = Vector2.zero;
-                transform.SetParent(hit.collider.transform);
+                transform.SetParent(topHit.collider.transform); //Attach to platform
+                shake.start = true;
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Z) && rigidBody.gravityScale == 0) {
+        if (Input.GetKeyUp(KeyCode.X) && rigidBody.gravityScale == 0) { //When player released magnetic key
             rigidBody.gravityScale = 1;
-            if (transform.parent != null) {
-                transform.SetParent(null);
+            transform.SetParent(null); //Detach from platform
+        }
+
+        bool isGrounded = GroundCheck();
+
+        if (isGrounded) {
+            float lastGroundTime = groundTimeRef;
+            groundTimeRef = Time.time;
+            if (groundTimeRef - lastGroundTime > .3f) { //Player has been airbourne for more than .3 seconds
+                shake.start = true;
             }
         }
     }
 
-    private Vector2 CalculateDirection(float angle) {
+    //8-Directional input clamp
+    private Vector2 ClampDirection(float angle) { 
         Vector2 direction = Vector2.zero;
         
         if (angle <= 10 && angle >= -10) direction = new Vector2(1, 0);
@@ -71,11 +67,15 @@ public class Player : MonoBehaviour
         return direction;
     }
 
-    private void Repel(Vector2 direction) {
-        rigidBody.linearVelocity = direction * power;
+    //Apply movement
+    private void Impulse(Vector2 direction, int impulseMode) {
+        Vector2 velocity = direction * impulseMode * power;
+        rigidBody.linearVelocity = velocity;
+        shake.start = true;
     }
 
-    private void Attract(Vector2 direction) {
-        rigidBody.linearVelocity = direction * power;
+    private bool GroundCheck() {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, platformLayer);
+        return hit.collider != null;
     }
 }
