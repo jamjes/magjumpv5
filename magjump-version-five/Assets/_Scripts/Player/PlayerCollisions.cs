0e9b40c41084bff0a980cf1c1d34e2b90c1e3cdd
Jam Jes <77445181+jamjes@users.noreply.github.com>
2025-01-28 15:13:31 +0000

    // New Collision Logic
    Previous:
    If magnet is pointing towards a platform under the player, a force will be applied in the opposite direction.
    
    New:
    If the player is grounded and the magnet is underneath the player, apply positive force. X value is controlled by magnet position relative to player position.

--- magjump-version-five/Assets/_Scripts/Player/PlayerCollisions.cs
+++ magjump-version-five/Assets/_Scripts/Player/PlayerCollisions.cs
@@ -1,38 +1,19 @@
 using UnityEngine;
 
-public class PlayerCollisions : MonoBehaviour
-{
-    public bool CanImpulse;
-    public bool CanMagnetise;
-    
+public class PlayerCollisions : MonoBehaviour {
+    public bool CanImpulse { private set; get; }
+    public bool CanMagnetise { private set; get; }
     [SerializeField] private LayerMask platformLayer;
-    [SerializeField] private float detectionLenience;
-    private BoxCollider2D coll;
+    [SerializeField] private float detectionLenience = .51f;
     private Rigidbody2D rbd;
 
-    private void Awake() {
-        coll = GetComponent<BoxCollider2D>();
-        rbd = GetComponent<Rigidbody2D>();
-    }
-
-    
-
     private void Update() {
-        CanImpulse = FloorCheck();
-        //CanMagnetise = CeilingCheck();
-
-        if (Input.GetKeyDown(KeyCode.X)) {
-            rbd.linearVelocityY = 15;
-        }
+        CanImpulse = PlatformCheck(Vector2.down);
+        CanMagnetise = PlatformCheck(Vector2.up);
     }
 
-    private bool FloorCheck() {
-        RaycastHit2D hit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0, Vector2.down, detectionLenience, platformLayer);
-        return hit.collider != null;
-    }
-
-    private bool CeilingCheck() {
-        RaycastHit2D hit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0, Vector2.up, detectionLenience, platformLayer);
+    private bool PlatformCheck(Vector2 direction) {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, detectionLenience, platformLayer);
         return hit.collider != null;
     }
 }
--- magjump-version-five/Assets/_Scripts/Player/PlayerMovement.cs
+++ magjump-version-five/Assets/_Scripts/Player/PlayerMovement.cs
@@ -1,84 +1,24 @@
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    private Vector3 mousePosition;
-    private Vector3 objectPosition;
-    private Vector2 direction;
-    private float angle;
-    [SerializeField] private Pivot pivot;
-    [SerializeField] private LayerMask platformLayer;
-    private SpriteRenderer spr;
-    private Rigidbody2D rb;
-    private BoxCollider2D coll;
-    [SerializeField] private PlayerAttributes attributes;
+public class PlayerMovement : MonoBehaviour {
+    private Rigidbody2D rbd;
+    [SerializeField] private Magnet magnet;
+    [SerializeField] private PlayerCollisions collisions;
 
     private void Awake() {
-        //spr = pivot.transform.GetChild(0).GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
-        coll = GetComponent<BoxCollider2D>();
+        rbd = GetComponent<Rigidbody2D>();
     }
 
-    void Update() {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction.normalized, attributes.detection_radius, platformLayer); //Player is within range to attract / repel
-        bool canImpulse;
-        if (hit.collider != null) {
-            pivot.SetColor(Color.green);
-            //spr.color = Color.green;
-            canImpulse = true;
-        }
-        else {
-            pivot.SetColor(Color.red);
-            //spr.color = Color.red;
-            canImpulse = false;
-        }
-
-        if (canImpulse && Input.GetKeyDown(KeyCode.X)) {
-            Vector2 impulseDirection = ClampDirection(angle);
-            Impulse(impulseDirection.normalized);
-        }
-
-        bool canStick = MagnetiseCheck();
-
-        if (canStick && Input.GetKey(KeyCode.X) && mousePosition.y > objectPosition.y) {
-            rb.gravityScale = 0;
-            rb.linearVelocity = Vector2.zero;
-        }
-        
-        if (Input.GetKeyUp(KeyCode.X)) {
-            rb.gravityScale = 1;
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.X)) {
+            CalculateDirection();
         }
     }
 
-    private bool MagnetiseCheck() {
-        RaycastHit2D hit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0, Vector2.up, 0, platformLayer);
-        return hit.collider != null;
-    }
-
-    private void Impulse(Vector2 direction) {
-        int impulseMode;
-        if (mousePosition.y > objectPosition.y) {
-            impulseMode = 1;
+    private void CalculateDirection() {
+        Vector2 magnetPos = magnet.transform.position;
+        if (magnetPos.y < transform.position.y && collisions.CanImpulse) {
+            rbd.linearVelocityY = 15f;
         }
-        else {
-            impulseMode = -1;
-        }
-        rb.linearVelocity = direction * impulseMode * attributes.power;
-    }
-
-    private Vector2 ClampDirection(float angle) {
-        Vector2 direction = Vector2.zero;
-
-        if (angle <= 10 && angle >= -10) direction = new Vector2(1, 0);
-        else if (angle < 80 && angle > 10) direction = new Vector2(.5f, .9f);
-        else if (angle <= 100 && angle >= 80) direction = new Vector2(0, 1);
-        else if (angle < 170 && angle > 100) direction = new Vector2(-.5f, .9f);
-        else if (angle <= -170 || angle >= 170) direction = new Vector2(-1, 0);
-        else if (angle < -100 && angle > -170) direction = new Vector2(-.5f, -.9f);
-        else if (angle <= -80 && angle >= -100) direction = new Vector2(0, -1);
-        else if (angle <= -10 && angle >= -80) direction = new Vector2(.5f, -.9f);
-
-        return direction;
     }
 }
