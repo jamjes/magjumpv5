e731f8b3918c3e283a3f8a6aa4a10873e6a5beea
Jam <77445181+jamjes@users.noreply.github.com>
2025-02-04 15:40:37 +0000

    //basic particle system

---
+++ magjump-version-five/Assets/ParticleController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem landParticles;
+    [SerializeField] private ParticleSystem magnetiseParticles;
+    [SerializeField] private LayerMask groundLayer;
+    private BoxCollider2D coll;
+
+    private void Awake() {
+        coll = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.layer != 6) {
+            return;
+        }
+
+        RaycastHit2D groundCheck = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0, Vector2.down, .1f, groundLayer);
+        RaycastHit2D ceilingCheck = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0, Vector2.up, .1f, groundLayer);
+
+        if (groundCheck.collider != null) {
+            landParticles.Play();
+        } else if (ceilingCheck.collider != null) {
+            magnetiseParticles.Play();
+        }
+
+    }
+}
